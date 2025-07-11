d47218ff228f67e56b3bd4eddf4c96c3a51706a7
U-1055 <filatov_truba@mail.ru>
2025-07-11 15:58:10 +0700

    Сделана инерция, переделан расчёт положения тела.

--- model.py
+++ model.py
@@ -1,53 +1,82 @@
 import tkinter as tk
 import time
 from threading import Thread
+
+import pylab as pl
+
 from physics_objects import Vector
 from base import g
+
 
 class Object:
     model: tk.Canvas
     id: int
-    weight: int
+    weight: float
     coords: list[float]
+    forces: list[Vector]
 
-    def __init__(self, model: tk.Canvas, id_: int, weight: int = 1):
+    def __init__(self, model: tk.Canvas, id_: int, weight: float = 1):
         self.model = model
         self.id = id_
         self.weight = weight
         self.coords = self.model.coords(self.id)
+        self.forces = [Vector(0, self.model.gravity_acceleration * weight), -Vector(0, self.model.resistance * weight)]  # ToDo: продумать вопрос с F сопротивления
 
     def delete(self):
         self.model.delete(self.id)
 
 
 class MoveableObject(Object):
-
-    speed: int
-    acceleration: int
+    """
+    Подвижный объект
+    :param speed: скорость объекта (м/с)
+    :param acceleration: ускорение объекта (м/с^2)
+    :param energy: энергия объекта (Дж) ToDo: какая энергия?
+    """
+    speed: Vector
+    acceleration: Vector
     energy: int
 
-    def __init__(self, model: tk.Canvas, id_: int, weight: int = 1):
+    def __init__(self, model: tk.Canvas, id_: int, weight: float = 1):
         super().__init__(model, id_, weight)
-        self.speed = 0
-        self.acceleration = 0
+        self.speed = Vector(0, 0)
+        self.acceleration = Vector(0, 0)
         self.energy = 0
 
-    def change_coords(self, changes: tuple[float, float]):
-        self.coords[0] += changes[0]
-        self.coords[1] += changes[1]
-        self.coords[2] += changes[0]
-        self.coords[3] += changes[1]
+        self.speeds = [0]
+        self.accelerations = [0]
+        self.times = [0.]
+
+    def change_coords(self, force: Vector, tick: float, time_: float):
+        print(self.forces, force.x, force.y)
+        self.acceleration = Vector(force.x / self.weight, force.y / self.weight)  # F=ma -> a = F/m
+        print(self.acceleration.y, self.speed.y)
+        self.speed = self.acceleration * 0.001 + self.speed  # V = V0 + a*dt
+        print(self.speed.y)
+
+        #new_speed = self.speed + force
+        self.coords[0] += self.speed[0]
+        self.coords[1] += self.speed[1]
+        self.coords[2] += self.speed[0]
+        self.coords[3] += self.speed[1]
         self.model.coords(self.id, *self.coords)
 
-        height = self.model.height - self.coords[3]
-        self.energy = (self.weight * self.speed ** 2) / 2
-        print(self.speed)
+        self.speeds.append(self.speed.y)
+        self.accelerations.append(self.acceleration.y)
+        self.times.append(time_)
+
     def lower_(self, points: int):
         """Сдвигает объект вниз на points точек"""
         coords = self.coords
         coords[1] += points
         coords[3] += points
         self.model.coords(self.id, *coords)
+
+    def plot_data(self):
+        pl.plot(self.times, self.speeds)
+        pl.show()
+        pl.plot(self.times, self.accelerations)
+        pl.show()
 
 
 class Model(tk.Canvas):
@@ -56,55 +85,70 @@ class Model(tk.Canvas):
     gravity_acceleration: float
     time_: float
     tick: float
+    resistance: float
 
-    def __init__(self, model_size: tuple[int, int], tick: int = 0.001, *args, **kwargs):
+    def __init__(self, model_size: tuple[int, int], tick: float = 0.001, gravity_acceleration: float = g, resistance: float = 0, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.width = model_size[0]
         self.height = model_size[1]
-        self.tick = self.time_= tick
+        self.tick = self.time_ = tick
+        self.resistance = resistance
         self._objects = []
         self.gravity_acceleration = g
 
+        self.processing = True
         thr = Thread(target=self.__event_loop)
         thr.start()
+        self.processing = False
 
     def __event_loop(self):
         """Цикл обработки событий"""
 
         while True:
+            if not self.processing:
+                continue
+
             time.sleep(self.tick)
             for obj in self._objects:  # Проход по списку объектов
 
                 if isinstance(obj, MoveableObject):  # Проверка и выбор объекта
                     for other_obj in self._objects:  # Проход по списку объектов
                         if not self.check_coords(obj.coords, other_obj.coords):   # проверка координат объекта
-                            obj.change_coords(self.__get_coord_vector(obj))
+                            obj.change_coords(self.__get_coord_vector(obj), self.tick, self.time_)
+                            self.create_oval(*self.get_center(obj.coords), *[x + 1 for x in self.get_center(obj.coords)])
                             if obj.coords[1] > self.height or obj.coords[0] > self.width or obj.coords[0] < 0 or obj.coords[3] < 0:
                                 print(obj, self.width, self.height, obj.coords)
                                 self._objects.remove(obj)
+                                obj.delete()
                                 break
                 self.time_ += self.tick
 
-    def add_object(self, obj_type, obj_id: int) -> Object:
+    def add_object(self, obj_type, obj_id: int, weight: float = 1) -> Object:
         """
         Добавляет объект в модель
         :param obj_type: тип объекта (Object или MoveableObject)
         :param obj_id: id объекта для его взаимодействия с tkinter.Canvas (возвращается методами Canvas: create_arc,
                        create_rectangle и т.п.)
+        :param weight: вес объекта (кг).
         :return:
         """
-        obj = obj_type(self, obj_id)
+        obj = obj_type(self, obj_id, weight)
         self._objects.append(obj)
         return obj
 
-    def __get_coord_vector(self, obj: MoveableObject) -> tuple[float, float]:
+    def __get_coord_vector(self, obj: MoveableObject) -> Vector:
         """"""
-        obj.acceleration = self.gravity_acceleration
-        obj.speed = obj.acceleration * self.time_
-        gravity_strength = Vector(0, 0.2)
-        wind = Vector(-1, -0.1)
-        res = gravity_strength + wind
-        return res.x, res.y
+        result_force = Vector(0, 0)
+        for force in obj.forces:
+            result_force += force
+
+        return result_force
+
+    def start_processing(self):
+        self.processing = True
+
+    def stop_processing(self):
+        self.processing = False
 
     @staticmethod
     def check_coords(coords1: list | tuple[int, int, int, int], coords2: tuple[int, int, int, int]) -> bool:
--- model_GUI.py
+++ model_GUI.py
@@ -1,5 +1,37 @@
-from tkinter import Tk, Frame
+import tkinter
+from tkinter import Tk, Button, Toplevel, Frame
 import model as mdl
+from physics_objects import Vector
+from threading import Thread
+import time
+
+
+class TestWindow(Toplevel):
+    model: mdl.Model
+
+    def __init__(self, model_: mdl.Model, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = model_
+        self.__place_widgets()
+
+    def __place_widgets(self):
+        main_frm = Frame(self)
+        main_frm.pack(fill=tkinter.BOTH, expand=True)
+
+        st_btn = Button(main_frm, text='Начать обработку', command=self.model.start_processing)
+        st_btn.grid(row=0, column=0)
+
+        stp_btn = Button(main_frm, text='Прервать обработку', command=self.model.stop_processing)
+        stp_btn.grid(row=0, column=1)
+
+        plot_btn = Button(main_frm, text='Получить данные об объектах', command=self.__plot_data)
+        plot_btn.grid(row=0, column=2)
+
+    def __plot_data(self):
+        for obj in self.model._objects:
+            if isinstance(obj, mdl.MoveableObject):
+                obj.plot_data()
+
 
 if __name__ == '__main__':
     root = Tk()
@@ -8,18 +40,25 @@ if __name__ == '__main__':
     root.geometry(f"{width}x{height}+{width // 2}+{height // 2}")
     root.title('Тестирование модели')
 
-    model = mdl.Model(model_size=(width, height))
+    model = mdl.Model(model_size=(width, height), resistance=0, tick=0.001)
     model.place(x=0, y=0, width=width, height=height)
-    model.add_object(mdl.Object, model.create_rectangle(0, height - 10, width, height, fill='Black'))
 
-    model.add_object(mdl.MoveableObject, model.create_oval(width // 2, 1, width // 2 + 25, 25))
-    model.add_object(mdl.MoveableObject, model.create_oval(width // 2, 25, width // 2 + 50, 50))
-    model.add_object(mdl.MoveableObject, model.create_oval(width // 2, 50, width // 2 + 75, 75))
-    model.add_object(mdl.MoveableObject, model.create_oval(width // 2, 75, width // 2 + 100, 100))
 
-    model.add_object(mdl.MoveableObject, model.create_oval(width // 2 + 60, 1, width // 2 + 75, 25))
-    model.add_object(mdl.MoveableObject, model.create_oval(width // 2 + 90, 1, width // 2 + 115, 25))
-    model.add_object(mdl.MoveableObject, model.create_oval(width // 2 - 30, 1, width // 2 - 5, 25))
-    model.add_object(mdl.MoveableObject, model.create_oval(width // 2 - 60, 1, width // 2 - 35, 25))
+    add_btn = Button(model, command=lambda: model.add_object(mdl.MoveableObject, model.create_oval(width // 2, 1, width // 2 + 25, 25)))
+    add_btn.pack()
+
+    model.add_object(mdl.Object, model.create_rectangle(0, height, width, height + 1, fill='Black'), 2)
+
+    a = model.add_object(mdl.MoveableObject, model.create_oval(width // 2, 0, width // 2 + 100, 100), weight=2)
+    b = model.add_object(mdl.MoveableObject, model.create_oval(width // 2 - 50, 0, width // 2 - 25, 100), weight=1)
+    c = model.add_object(mdl.MoveableObject, model.create_oval(width // 2 + 150, 0, width // 2 + 175, 100), weight=5)
+
+  #  root.after(100, lambda : a.forces.clear())
+   # root.after(100, lambda: a.forces.append(Vector(1, -1)))
+  #  root.after(101, lambda: a.forces.clear())
+
+    window = TestWindow(model)
+    window.geometry(f'500x500')
 
     root.mainloop()
+
